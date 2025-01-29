9c4e0731706a675fc79969dcbdf0799abab8c4ed
shehroz9712 <shehrysoftologics@gmail.com>
2025-01-30 01:42:06 +0500

    `Added social platforms to campaign creation and updated views to reflect changes`

--- database/migrations/2024_12_17_204003_create_social_plateforms_table.php
+++ database/migrations/2024_12_17_204003_create_social_plateforms_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('social_platforms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image');
             $table->boolean('status')->default(0);
             $table->timestamps();
         });
